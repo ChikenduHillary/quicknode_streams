5d4ba322f2cc64443a11501aee3c984dced7fe05
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2024-12-23 13:33:47 +0100

    fixed styling issue

--- src/App.jsx
+++ src/App.jsx
@@ -56,12 +56,14 @@ const MintMonitor = () => {
 
   return (
     <div className="h-fit min-h-screen w-full p-10 dark:bg-black  flex-col bg-gray-100  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Latest 20 Pump.Fun Mint Transactions
-      </p>
+      <div>
+        {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_2%,black)]"></div> */}
+        <p className="text-4xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          Latest 20 Pump.Fun Mint Transactions
+        </p>
+      </div>
 
-      <Card className="w-full max-w-5xl ">
+      <Card className="w-full max-w-5xl bg-stone-300">
         <CardHeader>
           <CardTitle>Recent Mints</CardTitle>
         </CardHeader>
@@ -81,7 +83,7 @@ const MintMonitor = () => {
                   return (
                     <div
                       key={i}
-                      className="flex justify-between items-center p-2 bg-secondary rounded"
+                      className="flex justify-between items-center bg-stone-200 p-2 bg-secondary rounded"
                     >
                       <div className="flex items-center space-x-2">
                         <a
