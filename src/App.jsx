b75b8e7db40b3bd26bd5f2b6e315f4b4b2101bb6
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2024-12-29 18:25:53 +0100

    added socket

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,9 @@ const MintMonitor = () => {
   };
 
   useEffect(() => {
-    const socket = io("http://localhost:3000", { transports: ["websocket"] });
+    const socket = io("https://quicknode-backend-ahdm.vercel.app", {
+      transports: ["websocket"],
+    });
 
     socket.on("connect", () => {
       console.log("Connected to the server.");
@@ -57,7 +59,6 @@ const MintMonitor = () => {
   return (
     <div className="h-fit min-h-screen w-full p-10 dark:bg-black  flex-col bg-gray-100  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div>
-        {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_2%,black)]"></div> */}
         <p className="text-4xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
           Latest 20 Pump.Fun Mint Transactions
         </p>
